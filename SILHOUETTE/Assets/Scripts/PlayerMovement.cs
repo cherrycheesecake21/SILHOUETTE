9544db1a921f3049db7deaf0558255182350f788
cherrycheesecake21 <124520112+cherrycheesecake21@users.noreply.github.com>
2024-07-03 14:16:47 -0400

    Rotation
    Trying to get rotation on the controller

--- SILHOUETTE/Assets/Scripts/PlayerMovement.cs
+++ SILHOUETTE/Assets/Scripts/PlayerMovement.cs
@@ -2,14 +2,24 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
+    //Establishing an object to call.
     public  Rigidbody rb;
 
+    //Establishing float values.
     public float forwardForce = 2000f;
     public float sidewaysForce = 2000f;
+    public float RotateSpeed = 3.0f;
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        // Here we are storing the value of the horizontal input.
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        //And here we call the rotate speed as we rotate the player, based on input.
+        transform.Rotate(Vector3.up * horizontalInput * RotateSpeed * Time.deltaTime);
+
+        // Here we are calling the player's input.
         if (Input.GetKey("s"))
         {
             rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
